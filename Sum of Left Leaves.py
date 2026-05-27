afab67bfd6b7116894fd019cd4cdbf2506116c33
Sharanya <ayapillasharanya@gmail.com>
2026-05-27 17:14:47 +0530

    Codes Codes

---
+++ Sum of Left Leaves.py
@@ -0,0 +1,14 @@
+class Solution:
+    def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        
+        total_sum = 0
+        
+        if root.left and not root.left.left and not root.left.right:
+            total_sum += root.left.val
+            
+        total_sum += self.sumOfLeftLeaves(root.left)
+        total_sum += self.sumOfLeftLeaves(root.right)
+        
+        return total_sum
